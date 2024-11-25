5c4c992ecfa6a684d32246201f0bbcb5f2d94aae
GabrielaSouza123 <00001091044685sp@al.educacao.sp.gov.br>
2024-11-24 22:43:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matematica',
+    'Quem foi pitagoras?',
+    'Um filósofo e matemático grego'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual estado tem a sigla Sp?',
+    'São Paulo'
+)
+
+criaCartao(
+    'Astronomia',
+    'O que o sol é?',
+    'Uma Estrela'
+)
+
+criaCartao(
+    'Educação Fisica',
+    'Quantos jogadores tem um time de Basquete?',
+    'duas equipes de cinco jogadores cada, mais sete reservas'
+)
